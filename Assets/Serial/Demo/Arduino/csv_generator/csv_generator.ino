f6b09a80105f92bdcc1aeea1a017849237a4bf10
Pierre Rossel <nospam.pierre.rossel@hesge.ch>
2014-02-03 14:20:03 +0100

    add flashing led to help debugging, data sending period can be changed at runtime

--- Assets/Serial/Demo/Arduino/csv_generator/csv_generator.ino
+++ Assets/Serial/Demo/Arduino/csv_generator/csv_generator.ino
@@ -1,10 +1,13 @@
 // Generate 4 values from 0 to 9999 and send them to the serial port
 // Values are separated by a TAB and sets are separated by a new line 
 
-String version = "1.1 / 2014-01-29 by Pierre Rossel";
+String version = "1.2 / 2014-02-03 by Pierre Rossel";
+
+#define LED 13
 
 int frame = 0;
 
+// See showHelp() for available modes doc
 typedef enum {
   AUTO, ECHO
 } 
@@ -12,9 +15,29 @@ Mode;
 
 Mode mode = AUTO;
 
+// Delay between each data sending in ms
+int dataPeriod = 100; 
+
+// When did we send the last data
+unsigned long lastDataMs = 0;
+
+// when did we last toggle the led
+unsigned long lastLedMs = 0;
+
+boolean ledValue = LOW;
+
+// time updated on each update
+unsigned long ms = 0;
+
+// Buffer the command while receiving data on serial line
 String cmd;
 
 void setup() { 
+
+  pinMode(LED, OUTPUT);
+
+  // Turn led on while waiting
+  setLed(HIGH);
 
   //Initialize serial and wait for port to open:
   Serial.begin(9600); 
@@ -22,9 +45,46 @@ void setup() {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
+  // turn led off after waiting
+  setLed(LOW);
+
 } 
 
+// Extract a token from a string.
+// Return the token or empty string if no more token
+// tokenPos: 0 based position of the token to return
+// delimiter: the char to delimit tokens
+// exemples: 
+// token("cmd param1 param2", ' ', 0) would return "cmd"
+// token("cmd param1 param2", ' ', 1) would return "param1"
+// token("cmd param1 param2", ' ', 2) would return "param2"
+// token("cmd param1 param2", ' ', 3) would return ""
+String token(String str, int tokenPos, char delimiter = ' ') {
+  int posStart = 0;
+  for (int iToken = 0; iToken <= tokenPos; iToken++) {
+
+    // look for next delimiter
+    int posDelim = str.indexOf(delimiter, posStart);
+    if (posDelim == -1) {
+      if (iToken == tokenPos)
+        return str.substring(posStart);
+      else
+        return "";
+    }
+    else if (iToken == tokenPos)
+      return str.substring(posStart, posDelim);
+
+    // TODO: look for next token start (support multiple separators)
+    posStart = posDelim + 1;
+  }
+
+  return "";
+}
+
 void loop() {
+
+  // update time
+  ms = millis();
 
   // Read serial for commands
   char c;
@@ -35,38 +95,46 @@ void loop() {
       Serial.write(c);
     }
 
-    switch (c) {
+    if (c == '\n') {
 
-    case '\n': 
       // try to interpret command
-      if (cmd == "auto")
+      String action = token(cmd, 0);
+
+      if (action == "auto") {
         mode = AUTO;
-      else if (cmd == "echo")
+        String period = token(cmd, 1);
+        if (period.length() > 0)
+          dataPeriod = period.toInt();
+      }
+      else if (action == "echo")
         mode = ECHO;
-      else if (cmd == "reset")
+      else if (action == "reset")
         frame = 0;
-      else if (cmd == "help") {
+      else if (action == "version")
+        Serial.println(version);
+      else if (action == "help") {
         showHelp();
       }
 
       cmd = "";
       break;
-
-    default:
-      cmd += c;
-
     }
 
+    else {
+      cmd += c;
+    }
   }
 
-  if (mode == AUTO) {
-    unsigned long ms = millis();
-    float freq = 0.2;
+  if (mode == AUTO && ms > lastDataMs + dataPeriod) {
+
+    lastDataMs = ms;
+
+    float sineFreq = 0.2;
 
     Serial.print(frame);
     Serial.print("\t");
 
-    Serial.print((int)(9999 * (sin(TWO_PI * freq * ms / 1000) / 2.0) ));
+    Serial.print((int)(9999 * (sin(TWO_PI * sineFreq * ms / 1000) / 2.0) ));
     //Serial.print(1000 + frame);
     Serial.print("\t");
 
@@ -77,25 +145,59 @@ void loop() {
     //Serial.print(random(9999));
     Serial.print(3000 + frame);
     Serial.println();
+
+    frame = (frame + 1) % 9999;
+
+    // Blink led (fast during 2 sec after reset, for each data afterwards)
+    if (ms > 2000) {
+      setLed(1);
+      //toggleLed();
+    }
+
+  }
+
+  // Blink led (5 times during 2 seconds after reset, for each data afterwards)
+  if (ms < 2000) {
+    if (ms > lastLedMs + 200) {
+      toggleLed();
+    }
+  }
+  else if (ledValue && ms > lastLedMs + 10) {
+    // turn off led shortly after sending data
+    setLed(0);
   }
 
   //delay(500);
-  delay(10);
+  //delay(10);
 
-  frame = (frame + 1) % 9999;
+}
+
+void setLed(boolean val) {
+  digitalWrite(LED, val);
+  ledValue = val;
+  lastLedMs = ms;
+}
+
+void toggleLed() {
+  ledValue = !ledValue;
+  lastLedMs = ms;
+  digitalWrite(LED, ledValue);
 }
 
 void showHelp() {
   Serial.println("");
   Serial.println("CSV Generator " + version);
   Serial.println("Available commands:");
-  Serial.println("auto");
+  Serial.println("auto [period]");
   Serial.println("    Sends continuously 4 int values separated by \\t and terminated by \\n.");
   Serial.println("    This is the default mode.");
+  Serial.println("    period (optional): delay [ms] between each line sending.");
   Serial.println("echo");
   Serial.println("    Sends back whatever it receives on its serial port.");
   Serial.println("reset");
   Serial.println("    Resets the frame count (the first value in auto mode).");
+  Serial.println("version");
+  Serial.println("    Sends version information.");
   Serial.println("help");
   Serial.println("    Shows help.");
   Serial.println("");
