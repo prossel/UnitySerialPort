c271c2b933b0e236611b1ca0a5bf592e4e386a26
Pierre Rossel <nospam.pierre.rossel@hesge.ch>
2018-04-13 17:33:51 +0200

    Make it work with Unity 2017.3 on OS X

--- Assets/Serial/Serial.cs
+++ Assets/Serial/Serial.cs
@@ -1,19 +1,19 @@
 /* 
- * Version 0.3.3, 2016-03-17, Pierre Rossel
+ * Version 0.4.0, 2018-04-13, Pierre Rossel
  * 
- * This behavior helps sending and receiving data from a serial port. 
+ * This component helps sending and receiving data from a serial port. 
  * It detects line breaks and notifies the attached gameObject of new lines as they arrive.
  * 
  * Usage 1: Receive data when you expect line breaks
  * -------
  * 
  * - drop this script to a gameObject
- * - create a script on the same gameObject to receive new line notifications
  * - select "Notify Lines" checkbox on Serial script
+ * - create a script on the same gameObject to receive new line notifications
  * - add the OnSerialLine() function, here is an example
  * 
  * 	void OnSerialLine(string line) {
- *		print "Got a line: " + line;
+ *		Debug.Log ("Got a line: " + line);
  *	}
  * 
  * Usage 2: Receive data (when you don't expect line breaks)
@@ -29,6 +29,15 @@
  * - from any script, call the static functions Serial.Write() or Serial.WriteLn()
  * - if not not already, the serial port will be opened automatically.
  * 
+ * Configuration
+ * -------------
+ * 
+ * Drop the SerialConfig component to an empty GameObject in your scene and configure:
+ * 
+ * - the preferred ports
+ * - the speed
+ * - whether you wand debug informations in console
+ * 
  * 
  * Troubleshooting
  * ---------------
@@ -37,7 +46,7 @@
  *     error CS0234: The type or namespace name `Ports' does not exist in the namespace `System.IO'. 
  *     Are you missing an assembly reference?
  * Solution: 
- *     File | Build Settings | Optimization | API Compatibility Level: .Net 2.0
+ *     Menu Edit | Project Settings | Player | Other Settings | API Compatibility Level: .Net 2.0
  * 
  */
 
@@ -45,14 +54,8 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+// If you get error CS0234 on this line, see troubleshooting section above
 using System.IO.Ports;
-
-// System.IO.Ports requires a working Serial Port. On Mac, you will need to purcase the Uniduino plug-in on the Unity Store
-// This adds a folder + a file into your local folder at ~/lib/libMonoPosixHelper.dylib
-// This file will activate your serial port for C# / .NET
-// The functions are the same as the standard C# SerialPort library
-// cf. http://msdn.microsoft.com/en-us/library/system.io.ports.serialport(v=vs.110).aspx
-
 
 public class Serial : MonoBehaviour
 {
@@ -93,14 +96,6 @@ public class Serial : MonoBehaviour
 	/// The values separator.
 	/// </summary>
 	public char ValuesSeparator = '\t';
-
-	/// <summary>
-	/// The enable debug infos.
-	/// The first script with debug infos enabled will enable them until the program stop. 
-	/// Therefore, only one script need to enable the debug info to have them in all app, even with multiple scene
-	/// and multiple instances, until the program stops.
-	/// </summary>
-	public bool EnableDebugInfos = false;
 
 	/// <summary>
 	/// The first line has been received.
@@ -146,28 +141,27 @@ public class Serial : MonoBehaviour
 
 	// flag to detect whether coroutine is still running to workaround coroutine being stopped after saving scripts while running in Unity
 	private int nCoroutineRunning = 0;
+
 	#endregion
 
 	#region Static vars
 
+	// Serial configuration
+	private static SerialConfig s_config = null;
+
 	// Only one serial port shared among all instances and living after all instances have been destroyed
 	private static SerialPort s_serial;
 
-	// 
+	// All instances of this component
 	private static List<Serial> s_instances = new List<Serial> ();
 
-	// Enable debug info
-	private static bool s_debug = false; // Do not change here. Use EnableDebugInfo on any script instance
+	// Enable debug info. Do not change here. Use parameter on SerialConfig component
+	private static bool s_debug = false;
 
 	private static float s_lastDataIn = 0;
 	private static float s_lastDataCheck = 0;
 
 	#endregion
-
-	void Start ()
-	{
-		// print ("Serial Start ");
-	}
 
 	void OnValidate ()
 	{
@@ -177,59 +171,52 @@ public class Serial : MonoBehaviour
 
 	void OnEnable ()
 	{
-		//		print("Serial OnEnable");
-		//		if (s_serial != null)
-		//			print ("serial IsOpen: " + s_serial.IsOpen);
-		//		else
-		//			print ("no serial: ");
-
 		s_instances.Add (this);
 
-		if (EnableDebugInfos && !s_debug) {
-			Debug.LogWarning("Serial debug informations enabled by " + this);
+		if (GetConfig ().logDebugInfos && !s_debug) {
+			Debug.LogWarning ("Serial debug informations enabled by " + GetConfig ());
 			s_debug = true;
 		}
 
-		checkOpen (9600);
-
+		checkOpen ();
 	}
 
 	void OnDisable ()
 	{
-		//print("Serial OnDisable");
 		s_instances.Remove (this);
 	}
 
 	public void OnApplicationQuit ()
 	{
-
 		if (s_serial != null) {
 			if (s_serial.IsOpen) {
-				print ("closing serial port");
+				if (s_debug) {
+					Debug.Log ("closing serial port");
+				}
 				s_serial.Close ();
 			}
 
 			s_serial = null;
 		}
-
 	}
 
 	void Update ()
 	{
-		//print ("Serial Update");
+		if (s_serial != null) {
 
-		if (s_serial != null && s_serial.IsOpen) {
+			// Will (re)open if device disconnected and reconnected (or Leonardo reset)
+			checkOpen ();
+
 			if (nCoroutineRunning == 0) {
 
-				//print ("starting ReadSerialLoop coroutine");
+				//Debug.Log ("starting ReadSerialLoop* coroutine from " + this.name);
 
 				switch (Application.platform) {
 
 				case RuntimePlatform.WindowsEditor:
 				case RuntimePlatform.WindowsPlayer:
-				case RuntimePlatform.WindowsWebPlayer:
-
-					s_serial.ReadTimeout = 1;
+//				case RuntimePlatform.OSXEditor:
+//				case RuntimePlatform.OSXPlayer:
 
 					// Each instance has its own coroutine but only one will be active
 					StartCoroutine (ReadSerialLoopWin ());
@@ -242,8 +229,11 @@ public class Serial : MonoBehaviour
 
 				}
 			} else {
-				if (nCoroutineRunning > 1)
-					print (nCoroutineRunning + " coroutines in " + name);
+				if (nCoroutineRunning > 1) {
+					if (s_debug) {
+						Debug.Log (nCoroutineRunning + " coroutines in " + name);
+					}
+				}
 
 				nCoroutineRunning = 0; 
 			}
@@ -256,19 +246,22 @@ public class Serial : MonoBehaviour
 		while (true) {
 
 			if (!enabled) {
-				//print ("behaviour not enabled, stopping coroutine");
+				if (s_debug) {
+					Debug.Log ("behaviour not enabled, stopping coroutine");
+				}
 				yield break; 
 			}
 
-			//print("ReadSerialLoop ");
+			//Debug.Log("ReadSerialLoop ");
 			nCoroutineRunning++; 
 
+			s_lastDataCheck = Time.time;
 			try {
-				s_lastDataCheck = Time.time;
 				while (s_serial.BytesToRead > 0) {  // BytesToRead crashes on Windows -> use ReadLine or ReadByte in a Thread or Coroutine
 
-
 					string serialIn = s_serial.ReadExisting ();
+
+					//Debug.Log("just read some data: " + serialIn);
 
 					// Dispatch new data to each instance
 					foreach (Serial inst in s_instances) {
@@ -279,7 +272,16 @@ public class Serial : MonoBehaviour
 				}
 
 			} catch (System.Exception e) {
-				print ("System.Exception in serial.ReadExisting: " + e.ToString ());
+				if (s_debug) {
+					Debug.LogError ("System.Exception in serial.ReadExisting: " + e.ToString ());
+					//Debug.Log ("isOpen: " + s_serial.IsOpen);
+				}
+			}
+
+			if (s_serial.IsOpen && s_serial.BytesToRead == -1) {
+				// This happens when Leonardo is reset
+				// Close the serial port here, it will be reopened later when available
+				s_serial.Close ();
 			}
 
 			yield return null;
@@ -289,40 +291,53 @@ public class Serial : MonoBehaviour
 
 	public IEnumerator ReadSerialLoopWin ()
 	{
+		if (s_debug) {
+			Debug.Log ("Start listening on com port: " + s_serial.PortName);
+		}
 
 		while (true) {
 
 			if (!enabled) {
-				//print ("behaviour not enabled, stopping coroutine");
+				Debug.Log ("behaviour not enabled, stopping coroutine");
 				yield break; 
 			}
 
-			//print("ReadSerialLoopWin ");
+			//Debug.Log ("ReadSerialLoopWin ");
 			nCoroutineRunning++; 
-			//print ("nCoroutineRunning: " + nCoroutineRunning);
+			//Debug.Log ("nCoroutineRunning: " + nCoroutineRunning);
+			Debug.Log ("Still listening on com port: " + s_serial.PortName + " open (" + s_serial.IsOpen + ") with coroutine from " + this);
 
 			string serialIn = "";
+			s_lastDataCheck = Time.time;
 			try {
+				s_serial.ReadTimeout = 1;
+				//Debug.Log ("isOpen: " + s_serial.IsOpen);
+
 				while (true) {  // BytesToRead crashes on Windows -> use ReadLine or ReadByte in a Thread or Coroutine
-					char c = (char)s_serial.ReadByte();
+					char c = (char)s_serial.ReadByte (); // ReadByte crashes on mac if device is removed (or Leonardo reset)
 					serialIn += c;
-
-					//serialIn += s_serial.ReadLine();
 				}
-
-			} catch (System.TimeoutException) {
-				//print ("System.TimeoutException in serial.ReadLine: " + te.ToString ());
+			} catch (System.TimeoutException e) {
+				Debug.Log ("System.TimeoutException in serial.ReadLine: " + e.ToString ());
 			} catch (System.Exception e) {
-				print ("System.Exception in serial.ReadLine: " + e.ToString ());
+				Debug.LogError ("System.Exception in serial.ReadLine: " + e.ToString ());
 			}
 
 			if (serialIn.Length > 0) {
 
 				//Debug.Log("just read some data: " + serialIn);
+
 				// Dispatch new data to each instance
 				foreach (Serial inst in s_instances) {
 					inst.receivedData (serialIn);
 				}
+				s_lastDataIn = s_lastDataCheck;
+			}
+
+			if (s_serial.BytesToRead == -1) {
+				// This happens when Leonardo is reset
+				// Close the serial port here, it will be reopened later when available
+				s_serial.Close ();
 			}
 
 			yield return null;
@@ -378,18 +393,20 @@ public class Serial : MonoBehaviour
 	/// <summary>
 	/// Act as if the serial port has received data followed by a new line.
 	/// </summary>
-	public void SimulateDataReceptionLn(float data) {
+	public void SimulateDataReceptionLn (float data)
+	{
 		foreach (Serial inst in s_instances) {
-			inst.receivedData(data + "\n");
+			inst.receivedData (data + "\n");
 		}
 	}
 
 	/// <summary>
 	/// Act as if the serial port has received data followed by a new line.
 	/// </summary>
-	public void SimulateDataReceptionLn(string data) {
+	public void SimulateDataReceptionLn (string data)
+	{
 		foreach (Serial inst in s_instances) {
-			inst.receivedData(data + "\n");
+			inst.receivedData (data + "\n");
 		}
 	}
 
@@ -398,29 +415,47 @@ public class Serial : MonoBehaviour
 	/// </summary>
 	/// <returns><c>true</c>, if port is opened, <c>false</c> otherwise.</returns>
 	/// <param name="portSpeed">Port speed.</param>
-	public static bool checkOpen (int portSpeed = 9600)
+	public static bool checkOpen ()
 	{
 
 		if (s_serial == null) {
+			s_serial = new SerialPort ();
+		}
 
+		if (!s_serial.IsOpen) {
+			
+			int portSpeed = GetConfig ().speed;
 			string portName = GetPortName ();
-
 			if (portName == "") {
-				print ("Error: Couldn't find serial port.");
+				if (s_debug) {
+					Debug.Log ("Error: Couldn't find serial port.");
+				}
 				return false;
 			} else {
-				if (s_debug)
-					print("Opening serial port: " + portName);
+				if (s_debug) {
+					Debug.Log ("Opening serial port: " + portName + " at " + portSpeed + " bauds");
+				}
 			}
 
-			s_serial = new SerialPort (portName, portSpeed);
+			s_serial.PortName = portName;
+			s_serial.BaudRate = portSpeed;
 
-			s_serial.Open ();
-			//print ("default ReadTimeout: " + s_serial.ReadTimeout);
-			//s_serial.ReadTimeout = 10;
+			try {
+				
+				s_serial.Open ();
+				s_serial.DtrEnable = true;
 
-			// clear input buffer from previous garbage
-			s_serial.DiscardInBuffer ();
+				//Debug.Log ("default ReadTimeout: " + s_serial.ReadTimeout);
+				//s_serial.ReadTimeout = 10;
+
+				// clear input buffer from previous garbage
+				s_serial.DiscardInBuffer ();
+
+			} catch (System.Exception e) {
+				if (s_debug) {
+					Debug.LogError ("System.Exception in serial.Open(): " + e.ToString ());
+				}
+			}
 		}
 
 		return s_serial.IsOpen;
@@ -438,7 +473,7 @@ public class Serial : MonoBehaviour
 		if (NotifyLines || NotifyValues) {
 
 			// prepend pending buffer to received data and split by line
-			string [] lines = (BufferIn + data).Split ('\n');
+			string[] lines = (BufferIn + data).Split ('\n');
 
 			// If last line is not empty, it means the line is not complete (new line did not arrive yet), 
 			// We keep it in buffer for next data.
@@ -455,8 +490,8 @@ public class Serial : MonoBehaviour
 					FirstLineReceived = true;
 
 					if (SkipFirstLine) {
-						if (EnableDebugInfos) {
-							Debug.Log("First line skipped: " + line);
+						if (s_debug) {
+							Debug.Log ("First line skipped: " + line);
 						}
 						continue;
 					}
@@ -469,7 +504,7 @@ public class Serial : MonoBehaviour
 					// trim lines buffer
 					int overflow = linesIn.Count - RememberLines;
 					if (overflow > 0) {
-						print ("Serial removing " + overflow + " lines from lines buffer. Either consume lines before they are lost or set RememberLines to 0.");
+						Debug.Log ("Serial removing " + overflow + " lines from lines buffer. Either consume lines before they are lost or set RememberLines to 0.");
 						linesIn.RemoveRange (0, overflow);
 					}
 				}
@@ -481,7 +516,7 @@ public class Serial : MonoBehaviour
 
 				// Notify values
 				if (NotifyValues) {
-					string [] values = line.Split (ValuesSeparator);
+					string[] values = line.Split (ValuesSeparator);
 					SendMessage ("OnSerialValues", values);
 				}
 
@@ -491,45 +526,85 @@ public class Serial : MonoBehaviour
 
 	static string GetPortName ()
 	{
+		SerialConfig config = GetConfig ();
 
-		string[] portNames;
+		if (s_debug) {
+			Debug.Log ("Prefered port names:\n" + string.Join ("\n", config.portNames));
+		}
+
+		List<string> portNames = new List<string> ();
 
 		switch (Application.platform) {
 
 		case RuntimePlatform.OSXPlayer:
 		case RuntimePlatform.OSXEditor:
-		case RuntimePlatform.OSXDashboardPlayer:
+		case RuntimePlatform.LinuxEditor:
 		case RuntimePlatform.LinuxPlayer:
 
-			portNames = System.IO.Ports.SerialPort.GetPortNames ();
+			portNames.AddRange (System.IO.Ports.SerialPort.GetPortNames ());
 
-			if (portNames.Length == 0) {
-				portNames = System.IO.Directory.GetFiles ("/dev/");                
+			if (portNames.Count == 0) {
+				portNames.AddRange (System.IO.Directory.GetFiles ("/dev/", "cu.*"));
 			}
+			break;
 
-			foreach (string portName in portNames) {                                
-				if (portName.StartsWith ("/dev/tty.usb") || portName.StartsWith ("/dev/ttyUSB"))
-					return portName;
-			}                
-			return "";
-
-		default: // Windows
-
-			portNames = System.IO.Ports.SerialPort.GetPortNames ();
-
-			// Defaults to last port in list (most chance to be an Arduino port)
-			if (portNames.Length > 0)
-				return portNames [portNames.Length - 1];
-			else
-				return "";
+		case RuntimePlatform.WindowsEditor:
+		case RuntimePlatform.WindowsPlayer:
+		default:
+			
+			portNames.AddRange (System.IO.Ports.SerialPort.GetPortNames ());
+			break;
 		}
+
+		if (s_debug) {
+			Debug.Log (portNames.Count + "available ports: \n" + string.Join ("\n", portNames.ToArray ()));
+		}
+
+		// Looking for preferred names in config
+		foreach (string name in config.portNames) {
+
+			string foundName = portNames.Find (s => s.Contains (name));
+			if (foundName != null) {
+				if (s_debug) {
+					Debug.Log ("Found port " + foundName);
+				}
+				return foundName;
+			}
+		}
+
+		// Defaults to last port in list (most chance to be an Arduino port)
+		if (portNames.Count > 0)
+			return portNames [portNames.Count - 1];
+		else
+			return "";
 	}
 
-	void OnGUI() {
+
+	static SerialConfig GetConfig ()
+	{
+	 	
+		if (s_config == null) {
+			s_config = GameObject.FindObjectOfType<SerialConfig> ();
+			if (!s_config) {
+				Debug.LogWarning ("Serial configuration not found. Using default values. To configure the prefered port names and speed, add the SerialConfig component to an empty GameObject", s_config);
+
+				// Provide a default config compatible with old version of UnitySerial
+				GameObject goConfig = new GameObject ("Serial configuration");
+				s_config = goConfig.AddComponent<SerialConfig> ();
+			}
+			DontDestroyOnLoad (s_config.gameObject);
+		}
+
+		return s_config;
+	}
+
+
+	void OnGUI ()
+	{
 
 		// Show debug only if enabled and by the first instance to avoid overwrite same data
-		if (s_debug && this == s_instances[0]) {
-			GUILayout.Label("Serial last data: " + s_lastDataIn + " (last check: " + s_lastDataCheck + ")");
+		if (s_debug && this == s_instances [0]) {
+			GUILayout.Label ("Serial last data: " + s_lastDataIn + " (last check: " + s_lastDataCheck + ")");
 		}
 	}
 
---
+++ Assets/TestSerial.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+
+public class TestSerial : MonoBehaviour
+{
+
+	public string portName;
+	public int portSpeed = 9600;
+
+	SerialPort serial;
+
+	// Use this for initialization
+	void Start ()
+	{
+		OpenPort();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (serial.IsOpen) {
+			
+			Debug.Log ("BytesToRead: " + serial.BytesToRead + "\n" +
+				"ReadBufferSize:  " + serial.ReadBufferSize + "\n" +
+				"Parity:  " + serial.Parity + "\n"+
+				"DtrEnable:  " + serial.DtrEnable + "\n"+
+				"RtsEnable:  " + serial.RtsEnable + "\n");
+
+			if (serial.BytesToRead >= 0) {
+				try {
+					serial.ReadTimeout = 1000;
+
+	//				int iByte = serial.ReadByte ();
+	//				Debug.Log (iByte);
+
+	//				int iChar = serial.ReadChar ();
+	//				Debug.Log (iChar);
+
+					string data = serial.ReadExisting();
+					Debug.Log(data);
+
+	//				char[] buffer = new char[10];
+	//				serial.Read(buffer, 0, 1);
+	//				Debug.Log(buffer);
+
+
+				} catch (System.Exception e) {
+					Debug.LogError(e);
+				}
+			}
+			else if (serial.BytesToRead == -1) {
+				// Happens when leonardo is reset, device disapears in system.
+				Debug.Log("is open: " + serial.IsOpen);
+
+				serial.Close();
+			}
+		}
+		else {
+			// try to open
+			//serial.Open();
+			OpenPort();
+		}
+	}
+
+	void OpenPort() {
+		if (serial == null) {
+			//serial = new SerialPort (portName, portSpeed, Parity.None, 8, StopBits.One);
+			serial = new SerialPort ();
+		}
+
+		if (serial.IsOpen) {
+			serial.Close();
+		}
+
+		// Get a list of available ports
+		List<string> portNames = new List<string> ();
+		portNames.AddRange (System.IO.Ports.SerialPort.GetPortNames ());
+		portNames.AddRange (System.IO.Directory.GetFiles ("/dev/", "cu.*"));
+		Debug.Log (portNames.Count + "available ports: \n" + string.Join ("\n", portNames.ToArray ()));
+
+		if (portName == "") {
+			// try with last port of the list
+			portName = portNames[portNames.Count - 1];
+		}
+
+		serial.PortName = portName;
+		serial.BaudRate = portSpeed;
+
+		try {
+			serial.Open ();
+			serial.DtrEnable = true; // Won't read from Leonardo without this
+		} catch (System.Exception e) {
+			Debug.LogError (e);
+		}
+
+		Debug.Log ("Port is open: " + serial.IsOpen);
+	}
+}
